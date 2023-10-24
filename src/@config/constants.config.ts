75ac0af3881f04ca31a3e2c46f55e1827f59e318
KhadkaRipesh <khadkanripesh@gmail.com>
2023-10-24 10:06:39 +0545

    jwt guard and getuser decorater

--- src/@config/constants.config.ts
+++ src/@config/constants.config.ts
@@ -7,3 +7,5 @@ export const DATABASE = {
   password: process.env.DB_PASSWORD!,
   database: process.env.DB_NAME!,
 };
+
+export const JWT_SECRET = process.env.JWT_SECRET!;
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JWT_SECRET } from 'src/@config/constants.config';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToekn(),
+      ignoreExpiration: false,
+      secretOrKey: JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.userRepository.findOne({
+      where: { id: payload.sub },
+    });
+
+    if (!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}

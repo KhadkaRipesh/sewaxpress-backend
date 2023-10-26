12ddf529cec7f2f0844895840631b4b1d0bd79b4
KhadkaRipesh <khadkanripesh@gmail.com>
2023-10-26 18:07:30 +0545

    add roles guard

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,10 @@
 import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+export enum UserRole {
+  admin = 'ADMIN',
+  seller = 'SELLER',
+  customer = 'CUSTOMER',
+}
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()

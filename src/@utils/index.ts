e5f1c0951ad75948e88c82531a9a6920fb443599
KhadkaRipesh <khadkanripesh@gmail.com>
2023-10-26 18:21:44 +0545

    enum for success message

--- src/@utils/index.ts
+++ src/@utils/index.ts
@@ -13,3 +13,20 @@ export interface errorResponse {
   source?: string | string[];
   description?: any;
 }
+
+export enum SuccessMessage {
+  CREATE = '%s created successfully.',
+  FETCH = '%s fetched successfully',
+  UPDATE = '%s updated successfully',
+  DELETE = '%s deleted successfully',
+  REMOVE = '%s removed successfully',
+  REGISTER = '%s registered successfully',
+  LOGGED_IN = 'logged in successfully',
+  STORED = '%s stored successfully',
+  PUBLISH = '%s published successfully',
+  VERIFY = '%s verified successfully',
+  REFRESH = '%s refreshed successfully',
+  DEACTIVATE = '%s deactivated',
+  ACTIVATE = '%s activated',
+  SENT = '%s  sent',
+}

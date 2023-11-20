102bb8ef09328c172802c1c1c9d07ac1f90ab964
KhadkaRipesh <khadkanripesh@gmail.com>
2023-11-20 17:50:05 +0545

    create user dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,24 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export enum GENDER {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  OTHERS = 'OTHERS',
+}
 export class CreateAccountDto {
   @IsString()
-  @IsnotEmpty()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  gender: GENDER;
 }

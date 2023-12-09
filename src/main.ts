a30306008de1d5c8fcb5f543bd83fcd19043fbb7
KhadkaRipesh <khadkanripesh@gmail.com>
2023-12-09 21:40:27 +0545

    swagger docs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { PORT } from './@config/constants.config';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './@filter/exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,25 @@ async function bootstrap() {
   );
   // Using filter for exception
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('sewaXpress')
+    .setDescription('Darta Sifaris API specification')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(PORT);
 }

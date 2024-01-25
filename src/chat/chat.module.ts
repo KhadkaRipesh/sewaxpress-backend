9b3ee2277965a415b0d47983ffcbbdde1bda75ca
KhadkaRipesh <khadkanripesh@gmail.com>
2024-01-25 18:38:29 +0545

    fixed tyeprom module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from './entities/chat.entity';
+import { Room } from './entities/room.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Chat, Room])],
   controllers: [ChatController],
-  providers: [ChatService]
+  providers: [ChatService],
 })
 export class ChatModule {}

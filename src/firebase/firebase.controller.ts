88a210f0268bb1acc14576727d52686c35645ae5
KhadkaRipesh <khadkanripesh@gmail.com>
2024-01-29 19:17:19 +0545

    firebase service for storing token

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -10,9 +10,52 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FirebaseService } from './firebase.service';
 import { JwtAuthGuard } from 'src/@guards/auth.guard';
 import { GetUser } from 'src/@decoraters/getUser.decorater';
+import {
+  CreateFirebaseNotificationTokenDto,
+  UpdateFirebaseNotificationTokenDto,
+} from './dto/firebase.dto';
+import { ResponseMessage } from 'src/@decoraters/response.decorater';
+import { SuccessMessage } from 'src/@utils';
 
 @ApiTags('Firebase')
 @Controller('firebase')
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) {}
+
+  // Save Token
+  @Post('save-token')
+  @ResponseMessage(SuccessMessage.STORED, 'Token')
+  @ApiOperation({ summary: 'Create new token' })
+  @UseGuards(JwtAuthGuard)
+  createToken(
+    @GetUser('id') user_id: string,
+    @Body() payload: CreateFirebaseNotificationTokenDto,
+  ) {
+    return this.firebaseService.createToken(user_id, payload);
+  }
+
+  // Update Token
+  @Patch('update-token/:token_id')
+  @ResponseMessage(SuccessMessage.UPDATE, 'Token')
+  @ApiOperation({ summary: 'Update token' })
+  @UseGuards(JwtAuthGuard)
+  updateToken(
+    @GetUser('id') user_id: string,
+    @Param('token_id') token_id: string,
+    @Body() payload: UpdateFirebaseNotificationTokenDto,
+  ) {
+    return this.firebaseService.updateToken(user_id, token_id, payload);
+  }
+
+  // Delete Token
+  @Patch('delete-token/:token_id')
+  @ResponseMessage(SuccessMessage.DELETE, 'Token')
+  @ApiOperation({ summary: 'Delete token' })
+  @UseGuards(JwtAuthGuard)
+  deleteToken(
+    @GetUser('id') user_id: string,
+    @Param('token_id') token_id: string,
+  ) {
+    return this.firebaseService.deleteToken(user_id, token_id);
+  }
 }
--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,4 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import * as firebase from 'firebase-admin';
+import { FIREBASE_SERVICE_ACCOUNT } from 'src/@config/constants.config';
+import { DataSource, In } from 'typeorm';
+import { FirebaseToken } from './entities/firebase-token.entity';
+import { CreateFirebaseNotificationTokenDto } from './dto/firebase.dto';
+import { Printer } from 'src/@helpers/printer';
 
 @Injectable()
-export class FirebaseService {}
+export class FirebaseService {
+  constructor(private readonly dataSource: DataSource) {
+    const firebaseConfig: firebase.ServiceAccount = {
+      projectId: FIREBASE_SERVICE_ACCOUNT.project_id,
+      clientEmail: FIREBASE_SERVICE_ACCOUNT.client_email,
+      privateKey: FIREBASE_SERVICE_ACCOUNT.private_key,
+    };
+    //check if firebase is already initialized if not then initialize it
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        credential: firebase.credential.cert(firebaseConfig),
+      });
+    }
+  }
+  // Save new token
+  async createToken(
+    user_id: string,
+    payload: CreateFirebaseNotificationTokenDto,
+  ) {
+    await this.dataSource.manager.save(FirebaseToken, {
+      user_id,
+      device_type: payload.device_type,
+      notification_token: payload.notification_token,
+    });
+    return { message: 'Token saved.' };
+  }
+  // Update token
+  async updateToken(
+    user_id: string,
+    token_id: string,
+    payload: CreateFirebaseNotificationTokenDto,
+  ) {
+    await this.dataSource.manager.update(
+      FirebaseToken,
+      { id: token_id, user_id },
+      payload,
+    );
+    return { message: 'Token updated.' };
+  }
+  // Delete token
+  async deleteToken(user_id: string, token_id: string) {
+    await this.dataSource.manager.update(
+      FirebaseToken,
+      { id: token_id, user_id },
+      { is_active: false },
+    );
+    return { message: 'Token deleted.' };
+  }
+
+  //   Send push notification
+  async sendPushNotifications(
+    user_ids: string[],
+    payload: { title: string; body: string },
+  ) {
+    const tokens = await this.dataSource.manager.find(FirebaseToken, {
+      where: { user_id: In(user_ids), is_active: true },
+    });
+    for (const token of tokens) {
+      await firebase
+        .messaging()
+        .send({
+          token: token.notification_token,
+          notification: {
+            title: payload.title,
+            body: payload.body,
+          },
+        })
+        .catch((error) => {
+          Printer('FIREBASE NOTIFICATION ERROR: ', error);
+        });
+    }
+    return { message: 'Push notification sent.' };
+  }
+}

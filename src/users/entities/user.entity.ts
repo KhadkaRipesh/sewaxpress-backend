acf3e836172878f821cb710964638abfa3fc5d11
KhadkaRipesh <khadkanripesh@gmail.com>
2024-02-01 19:49:40 +0545

    service provider payment details

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -7,6 +7,7 @@ import { HubReview } from 'src/hub/entities/hub-review.entity';
 import { Hub } from 'src/hub/entities/hub.entity';
 import { Notification } from 'src/notification/entities/notification.entity';
 import { OTP } from 'src/otp/entities/otp.entity';
+import { ServiceProviderPaymentDetail } from 'src/service_provider/entities/service_provider-payment.entity';
 import {
   Column,
   CreateDateColumn,
@@ -104,4 +105,9 @@ export class User {
     cascade: true,
   })
   notifications: Notification[];
+
+  @OneToMany(() => ServiceProviderPaymentDetail, (payment) => payment.user, {
+    cascade: true,
+  })
+  service_provider_payment_detail: ServiceProviderPaymentDetail[];
 }
